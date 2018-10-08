fa2ca84244602c4dde80c46d0362745b8db8a48f
liverpoljer <liverpoljer@live.dk>
2018-10-08 11:23:10 +0200

    entities

---
+++ WebShopSpace/SpaceTicket.Core.Entity/SpaceTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SpaceTicket.Core.Entity
+{
+    class SpaceTicket
+    {
+        public double TicketPrice { get; set; }
+        public DateTime TravelDate { get; set; }
+        public DateTime TravelTime { get; set; }
+        public double TravelDistance { get; set; }
+        public String PictureURL { get; set; }
+        public int TicketLeft { get; set; }
+        public int TotalTicket { get; set; }
+
+
+    }
+}
---
+++ WebShopSpace/SpaceTicket.Core.Entity/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTicket.Core.Entity
+{
+    class User
+    {
+        public Boolean Admin { get; set; }
+        public String Username { get; set; }
+        public String Passsword { get; set; }
+        public String EmailAddress { get; set; }
+    }
+}
